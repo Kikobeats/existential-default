e832848739ffb5fb0190f96af1ae859319f42fb4
Kiko Beats <josefrancisco.verdu@gmail.com>
2015-06-22 10:51:10 +0200

    Revert "Revert "1.0.1 releases""
    This reverts commit 50a9a1a52690cf6ce8546dfb35b01df463eb19d8.

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 'use strict';
 
-var exists = require('existential');
-
 module.exports = function(value, defaultValue) {
-  return exists(value) ? value : defaultValue;
+  return value || defaultValue;
 };
